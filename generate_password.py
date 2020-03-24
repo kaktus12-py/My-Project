e3add1e08577125cf2f34eab46da4f316ef789de
kaktus12-py <58419599+kaktus12-py@users.noreply.github.com>
2020-03-24 09:36:56 +0700

    Update generate_password.py

--- generate_password.py
+++ generate_password.py
@@ -1,1 +1,16 @@
-from random import choicesdef generate_password(m):    SYMBOLS = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789'    a = choices(SYMBOLS, k=m)    return ''.join(a)def main(n, m):    password_set = set()    while len(password_set) < n:        password_set.add(generate_password(m))    return list(password_set)# print(*main(1, 1), sep="\n")
+from random import choices
+from string import ascii_letters, digits
+
+def generate_password(m):
+    SYMBOLS = ascii_letters + digits
+    a = choices(SYMBOLS, k=m)
+    return ''.join(a)
+
+
+def main(n, m):
+    password_set = set()
+    while len(password_set) < n:
+        password_set.add(generate_password(m))
+    return list(password_set)
+
+# print(*main(1, 1), sep="\n")
